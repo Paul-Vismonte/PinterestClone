8ca84ab10dd73b532dc03151eb7953aee15dc6c3
Mangulabnan-Bernard <adaiki287@gmail.com>
2025-02-21 01:28:11 +0800

    home page for a pintereClone

---
+++ lib/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'main.dart';
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Reddit', style: TextStyle(color: CupertinoColors.systemOrange, fontSize: 20, fontWeight: FontWeight.bold),),
+        trailing: CupertinoButton(child: Icon(CupertinoIcons.settings, color: CupertinoColors.systemOrange,), onPressed: (){
+          showCupertinoDialog(context: context, builder: (context){
+            return CupertinoAlertDialog(
+              title: Text('Logout'),
+              content: Text('Are you sure?'),
+              actions: [
+                CupertinoButton(child: Text("Yes"), onPressed: (){
+                  Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context)=> MyApp()));
+                }),
+                CupertinoButton(child: Text("No", style: TextStyle(color: CupertinoColors.destructiveRed),), onPressed: (){
+                  Navigator.pop(context);
+                })
+              ],
+            );
+          });
+        }),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SafeArea(child: Column(
+          children: [
+            Text ('Homepage')
+          ],
+        )),
+      ),
+    );
+  }
+}
